992065727c18a0330bf7a6fba48708df601a93b9
xydoy92 <xydoy92@gmail.com>
2020-07-28 16:14:09 +0300

    fix: delete prompts from function declaration

--- changedArrayFunctionTask.js
+++ changedArrayFunctionTask.js
@@ -1,9 +1,6 @@
 let array = [];
 	// Добавил ещё два параметра, потому что так функция кажется более переиспользуемой
-function getChangedArray(array,
-						amount = +prompt("Сколько чисел вы хотите в массиве?"),
-						moreOrLess = confirm("Увеличить(ОК) или уменьшить(Отмена) массив?"),
-						factor = +prompt("Во сколько увеличить/уменьшить массив?")) {
+function getChangedArray(array, amount, moreOrLess, factor) {
 	while(array.length < amount) {
 		array.push(+prompt("Введите число массива"));
 	}
@@ -13,7 +10,10 @@ function getChangedArray(array,
 	return array.map(item => item/factor);
 }
 
-let changedArray = getChangedArray(array);
+let userAmount = +prompt("Сколько чисел вы хотите в массиве?");
+let moreOrLess = confirm("Увеличить(ОК) или уменьшить(Отмена) массив?");
+let userFactor = +prompt("Во сколько увеличить/уменьшить массив?");
+let changedArray = getChangedArray(array, userAmount, moreOrLess, userFactor);
 
 console.log(array);
 console.log(changedArray);
